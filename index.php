9d32042ff315f193f33b28978958f99fb9604f74
opeomotayo <opeomotayo@gmail.com>
2015-10-30 13:32:57 +0000

    Formatted index.php

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
 				<p>
 					<a href="create.php" class="btn btn-success">Create</a>
 				</p>
-				
+
 				<table class="table table-striped table-bordered">
-		              <thead>
+		        <thead>
 		                <tr>
 		                  <th>Name</th>
 		                  <th>Email Address</th>
@@ -26,26 +26,26 @@
 		                </tr>
 		              </thead>
 		              <tbody>
-		              <?php 
-					   include 'database.php';
-					   $pdo = Database::connect();
-					   $sql = 'SELECT * FROM customers ORDER BY id DESC';
-	 				   foreach ($pdo->query($sql) as $row) {
-						   		echo '<tr>';
-							   	echo '<td>'. $row['name'] . '</td>';
-							   	echo '<td>'. $row['email'] . '</td>';
-							   	echo '<td>'. $row['mobile'] . '</td>';
-							   	echo '<td width=250>';
-							   	echo '<a class="btn" href="read.php?id='.$row['id'].'">Read</a>';
-							   	echo '&nbsp;';
-							   	echo '<a class="btn btn-success" href="update.php?id='.$row['id'].'">Update</a>';
-							   	echo '&nbsp;';
-							   	echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Delete</a>';
-							   	echo '</td>';
-							   	echo '</tr>';
-					   }
-					   Database::disconnect();
-					  ?>
+		              <?php
+include 'database.php';
+$pdo = Database::connect();
+$sql = 'SELECT * FROM customers ORDER BY id DESC';
+foreach ($pdo->query($sql) as $row) {
+    echo '<tr>';
+    echo '<td>' . $row['name'] . '</td>';
+    echo '<td>' . $row['email'] . '</td>';
+    echo '<td>' . $row['mobile'] . '</td>';
+    echo '<td width=250>';
+    echo '<a class="btn" href="read.php?id=' . $row['id'] . '">Read</a>';
+    echo '&nbsp;';
+    echo '<a class="btn btn-success" href="update.php?id=' . $row['id'] . '">Update</a>';
+    echo '&nbsp;';
+    echo '<a class="btn btn-danger" href="delete.php?id=' . $row['id'] . '">Delete</a>';
+    echo '</td>';
+    echo '</tr>';
+}
+Database::disconnect();
+?>
 				      </tbody>
 	            </table>
     	</div>
